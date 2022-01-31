e44b68cce638b79c80b1459105f09fcda6040fc8
parsarezaee <parsarezaee777@yahoo.com>
2022-01-31 13:51:08 +0330

    fix bug Room class

--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Room(models.Model):
     name = models.CharField(max_length=30)
-    description = models.TextField(max_length=100)
+    description = models.TextField(null=True, blank=True)
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
     slug = models.SlugField()
@@ -11,3 +11,4 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
--- home/views.py
+++ home/views.py
@@ -6,13 +6,10 @@ rooms = [
     {'id': 1, 'name':'python challenge'},
     {'id': 2, 'name':'cpp challenge in 100 days'},
     {'id': 3, 'name':'golang challenge in 50 days'}
-
-
-
-
 ]
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'home/home.html', {'rooms':rooms})
 
 
