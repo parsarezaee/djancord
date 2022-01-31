8943b2960ac483901eb1017028e53db9674e27a7
parsarezaee <parsarezaee777@yahoo.com>
2022-01-31 14:00:58 +0330

    fix views bug

--- home/views.py
+++ home/views.py
@@ -2,20 +2,11 @@ from django.shortcuts import render
 from .models import Room
 
 
-rooms = [
-    {'id': 1, 'name':'python challenge'},
-    {'id': 2, 'name':'cpp challenge in 100 days'},
-    {'id': 3, 'name':'golang challenge in 50 days'}
-]
-
 def home(request):
     rooms = Room.objects.all()
     return render(request, 'home/home.html', {'rooms':rooms})
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     return render(request, 'home/room.html', {'room':room})
