1c5e1b19daa8bf195a6817544ccabe88dbf828ae
parsarezaee <parsarezaee777@yahoo.com>
2022-01-31 19:14:06 +0330

    add update form feature

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
-    path('create-room/', views.createRoom, name='create-room') 
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<str:pk>', views.updateRoom, name='update-room') 
 ]
--- home/views.py
+++ home/views.py
@@ -22,3 +22,14 @@ def createRoom(request):
             return redirect('home:home')
             
     return render(request, 'home/room_form.html', {'form':form})
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home:home')
+    return render(request, 'home/room_form.html', {'form':form})
