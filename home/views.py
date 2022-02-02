8f0fc5627a1212c557387c7a9d791d1fa21c63fb
parsarezaee <parsarezaee777@yahoo.com>
2022-02-02 20:45:07 +0330

    add search bar

--- home/views.py
+++ home/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
 from django.views import View
-
+from django.db.models import Q
 
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
 
     topics = Topic.objects.all()
 
